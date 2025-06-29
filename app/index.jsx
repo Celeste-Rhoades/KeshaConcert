f1fc2726308b5b09f99fe9440501061690bd3eb8
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-06-28 21:27:19 -0600

    added nativeWind

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "../constants/styles";
+import "./index.css";
 import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
 import { Stack } from "expo-router";
 
@@ -7,42 +7,55 @@ export default function HomeScreen() {
   const [count, setCount] = useState(0);
 
   return (
-    <ImageBackground
-      source={require("../assets/images/background.png")}
-      style={styles.background}
-    >
-      <Stack.Screen options={{ title: "Counter App" }} />
-      <View style={styles.container}>
-        <Text style={styles.counter}>{count}</Text>
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: false, // Hide the default header
+        }}
+      />
+      <ImageBackground
+        source={require("../assets/images/background.png")}
+        className="flex-1"
+        style={{ flex: 1, width: "100%", height: "100%" }}
+        resizeMode="cover"
+      >
+        <View className="flex-1">
+          <Text className="text-8xl font-extrabold text-center mt-24 text-[#e982d1]">
+            Tits Out Tour
+          </Text>
+          <View className="flex-1 justify-center items-center">
+            <Text className="text-6xl font-bold mb-10 text-white">{count}</Text>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setCount(count + 2)}
-        >
-          <Text style={styles.buttonText}>+2</Text>
-        </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-neutral-800 p-4 rounded-xl my-2 w-36 items-center"
+              onPress={() => setCount(count + 2)}
+            >
+              <Text className="text-white text-xl">+2</Text>
+            </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setCount(count - 1)}
-        >
-          <Text style={styles.buttonText}>-1</Text>
-        </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-neutral-800 p-4 rounded-xl my-2 w-36 items-center"
+              onPress={() => setCount(count - 1)}
+            >
+              <Text className="text-white text-xl">-1</Text>
+            </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setCount(count + 3)}
-        >
-          <Text style={styles.buttonText}>+3</Text>
-        </TouchableOpacity>
+            <TouchableOpacity
+              className="bg-neutral-800 p-4 rounded-xl my-2 w-36 items-center"
+              onPress={() => setCount(count + 3)}
+            >
+              <Text className="text-white text-xl">+3</Text>
+            </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.resetButton}
-          onPress={() => setCount(0)}
-        >
-          <Text style={styles.buttonText}>Reset</Text>
-        </TouchableOpacity>
-      </View>
-    </ImageBackground>
+            <TouchableOpacity
+              className="bg-red-500 p-4 rounded-xl mt-8 w-36 items-center"
+              onPress={() => setCount(0)}
+            >
+              <Text className="text-white text-xl">Reset</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </>
   );
 }
